bb4b9496e543964abaea7e56b0661cbbfdf7cee1
Jose Hiraldo <hiraldo06@gmail.com>
2017-05-02 21:19:27 -0400

    Creando proyecto Subiendo por primera vez proyecto control de versiones

---
+++ src/main/java/com/jaha/ControlArchivoApplication.java
@@ -0,0 +1,12 @@
+package com.jaha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlArchivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlArchivoApplication.class, args);
+	}
+}
